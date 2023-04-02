dae403beb8c59c0b47fe3b5bd6c9b4486ebc3dc9
RAKESH KUMAR <rakeshbimra@outlook.com>
2023-04-02 19:24:44 +0530

    Add project files.

---
+++ JwtWithClientCredentialsDemo.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,54 @@
+﻿using JwtWithClientCredentialsDemo.Application.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtWithClientCredentialsDemo.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtSettings _jwtSetingsOption;
+
+        public JwtTokenGenerator(IOptions<JwtSettings> jwtSetings)
+        {
+
+            _jwtSetingsOption = jwtSetings.Value;
+
+        }
+        public (string access_token, string token_type, DateTime expires_in) GenerateToken(Guid userId, string userName, IList<string> userRoles)
+        {
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_jwtSetingsOption.Secret)),
+                    SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,userId.ToString()),
+                new Claim(ClaimTypes.Name,userName),
+                new Claim (JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+
+            var securityToken = new JwtSecurityToken(
+                issuer: _jwtSetingsOption.Issuer,
+                audience: _jwtSetingsOption.Audience,
+                expires: DateTime.Now.AddMinutes(_jwtSetingsOption.ExpiryMinutes),
+                claims: claims,
+                signingCredentials: signingCredentials);
+
+            return (new JwtSecurityTokenHandler().WriteToken(securityToken), "Bearer", securityToken.ValidTo);
+        }
+    }
+}
---
+++ JwtWithClientCredentialsDemo.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWithClientCredentialsDemo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+    }
+}
